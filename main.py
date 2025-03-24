fdd85358527215d444155155f0a2ea61f0781f32
Rajni <rajnikasotiya1993@gmail.com>
2025-03-24 23:25:43 +0530

    index route added

--- main.py
+++ main.py
@@ -6,7 +6,12 @@ def read_root():
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}                 
+    return {"item_id": item_id, "q": q}    
+
+#index route added
+@app.get("/root")
+def my_index_root():
+    return {"Hello": "World"}                
 
 
 if __name__ == "__main__":

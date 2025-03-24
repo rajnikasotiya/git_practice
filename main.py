3ab4c6fe1da51a615d12534bdfa3c89ed5148a2b
Rajni <rajnikasotiya1993@gmail.com>
2025-03-24 23:17:15 +0530

    item route added

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}                 
 
 
 if __name__ == "__main__":
